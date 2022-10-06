6f6441ac711b176b23730aaff7ab2801d48a018d
Alec Summers <alecsummers@Alecs-iMac.local>
2022-10-06 11:43:58 -0600

    Had to restart

--- Develop/script.js
+++ Develop/script.js
@@ -10,24 +10,5 @@ function writePassword() {
 
 }
 
-function generatePassword() {
-  var lowercase = "abcdefghijklmnopqrstuvwxyz"
-  var password = ""
-  for (let i = 0; i < 10; ++i) {
-    var index = Math.floor(Math.random() * lowercase.length);
-    var char = lowercase[index]
-    password = password + char
-
-
-
-
-
-  }
-
-  console.log(password)
-
-
-  return password;
-}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
